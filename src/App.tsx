6e429362cb4e32b168a1fed36d393654a95f18aa
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-04-04 17:28:33 -0500

    Make a dummy fetch request to arena-influences; Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
 import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
 
+import { useState, useEffect } from "react";
+
+import { Channel, Content } from "./types";
+
+const fetchChannelUrl = "http://api.are.na/v2/channels/arena-influences/thumb";
+
 function App() {
+  const [channelData, setChannelData] = useState<Content[]>([]);
+
+  useEffect(() => {
+    const getDummyData = async () => {
+      try {
+        const response = await fetch(fetchChannelUrl);
+        if (!response.ok) {
+          throw new Error("Network error with fetching channel thumb");
+        }
+        const json: Channel = await response.json();
+        setChannelData(json.contents);
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error(err.message);
+        } else {
+          throw err;
+        }
+      }
+    };
+
+    getDummyData();
+  }, []);
+
   return (
-    <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw />
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "grid",
+        gridTemplateColumns: "2fr 1fr",
+      }}
+    >
+      <div style={{}}>
+        <Tldraw />
+      </div>
+      <div style={{ padding: "20px" }}>
+        <ul>
+          {channelData.map((elt, idx) => (
+            <li key={idx}>{elt.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
